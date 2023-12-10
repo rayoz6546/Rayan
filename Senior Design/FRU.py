52918b9555075ef552c8f7cce7925c00b507c344
rayanhassan <99844081+rayoz6546@users.noreply.github.com>
2023-12-10 16:37:57 -0500

    FRU.py

--- Senior Design/FRU.py
+++ Senior Design/FRU.py
@@ -3,9 +3,6 @@ import os
 from PIL import Image
 import joblib 
 
-
-
-
 def load_images(folder_path):
     images = []
     for filename in os.listdir(folder_path):
@@ -13,26 +10,17 @@ def load_images(folder_path):
             img = Image.open(os.path.join(folder_path, filename)).convert('L')  
             img_array = np.array(img.resize((100, 100))).flatten()  
             images.append(img_array)
-
-
     return np.array(images)
-
 
 def FRU():
     input_dir = "/home/ece1896team2/Documents/SeniorDesign/Images_Live"
-
-
     verified = False
     recognizer= joblib.load('/home/ece1896team2/Documents/SeniorDesign/svm_model.joblib')
-
     images = []
     os.chdir(input_dir)
-
     X_live = []
-
     images_live = load_images(input_dir)
     X_live.extend(images_live)
-
     y_predict = recognizer.predict(X_live)
 
     majority = np.bincount(y_predict).argmax()
@@ -42,7 +30,6 @@ def FRU():
     else:
         print("UNVERIFIED")
         verified = False
-        
     return verified
 
 
